fa3f49960dfed800d9813910e7296e44665c0f2b
cosmin savu <savucosmin9183@gmail.com>
2020-03-19 10:55:47 +0200

    delete

--- lab3pw.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const myRoute = require('./route.js');
-const app = express();
-
-app.use('/books', myRoute);
-
-
-
-app.listen(3000, () => {console.log('App listening on port 3000');});
--- start.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const myAwesomeRoute = require('./route.js');
- 
-const app = express();
- 
-app.use('/hello', myAwesomeRoute);
- 
-app.listen(3000, () => {console.log('App listening on port 3000');})
